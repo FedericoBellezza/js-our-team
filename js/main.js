77af0682f4197c529db5f42046868eae842eeb47
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-10-29 17:06:20 +0100

    add member section

--- js/main.js
+++ js/main.js
@@ -37,10 +37,11 @@ const teamMembers = [
 	},
 ];
 
-// const imgEl = document.getElementById('img');
-// const nameEl = document.getElementById('name');
-// const roleEl = document.getElementById('role');
-// const emailEl = document.getElementById('email');
+const formNameEl = document.getElementById('form-name');
+const formRoleEl = document.getElementById('form-role');
+const formEmailEl = document.getElementById('form-email');
+const formImgEl = document.getElementById('form-img');
+const formEl = document.getElementById('form');
 const cardContainerEl = document.getElementById('card-container');
 
 for (let i = 0; i < teamMembers.length; i++) {
@@ -62,3 +63,30 @@ for (let i = 0; i < teamMembers.length; i++) {
   </div>
   </div>`;
 }
+
+const addMember = () => {
+	name = formNameEl.value;
+	role = formRoleEl.value;
+	email = formEmailEl.value;
+	img = formImgEl.value;
+	cardContainerEl.innerHTML += `
+  <div class="col-4 g-0 card text-bg-dark rounded-0 border-0">
+  <div class="row g-0">
+  <div class="col-md-4">
+  <img id="img" src="./${img}" class="img-fluid" alt="..." />
+  </div>
+  <div class="col-md-8">
+  <div class="card-body text-start">
+  <h4 id="name" class="card-title">${name}</h4>
+  <p id="role" class="card-text mb-4 h5">${role}</p>
+  <p id="email" class="card-text text-primary h5">${email}</p>
+  </div>
+  </div>
+  </div>
+  </div>`;
+};
+
+formEl.addEventListener('submit', (e) => {
+	e.preventDefault();
+	addMember();
+});
